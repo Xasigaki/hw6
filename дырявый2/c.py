960510a259acd8af24b55f40e71bdfb43fc20e14
Xasigaki <nursi.07060504@gmail.com>
2022-12-25 16:09:09 +0000

    hw6

---
+++ дырявый2/c.py
@@ -0,0 +1,7 @@
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+print(Fore.GREEN + 'КОРОЧЕ ГОВОРЯ')
+print(Back.YELLOW + 'ЭТО ИСПЫТАНИЕ')
+print(Style.BRIGHT + 'КОЛОРАМЫ' + Style.RESET_ALL)
+print('IS IT TEXT')
